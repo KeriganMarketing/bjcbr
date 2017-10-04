b937d0e446e66fcd6239447623fe266d06ee09cc
Daron Adkins <daron.adkins@gmail.com>
2017-10-04 10:33:54 -0500

    format

--- wp-content/themes/kma-slim/functions.php
+++ wp-content/themes/kma-slim/functions.php
@@ -6,10 +6,10 @@
  * @version 1.2
  */
 
-use Includes\Modules\Social\SocialSettingsPage;
 use Includes\Modules\Layouts\Layouts;
-use Includes\Modules\Slider\BulmaSlider;
 use Includes\Modules\Team\Physicians;
+use Includes\Modules\Slider\BulmaSlider;
+use Includes\Modules\Social\SocialSettingsPage;
 
 require('vendor/autoload.php');
 
@@ -31,11 +31,10 @@ $physicians->createPostType();
 $physicians->createAdminColumns();
 
 if (is_admin()) {
-
-    $post_id = (isset ($_GET['post']) ? $_GET['post'] : (isset ($_POST['post_ID']) ? $_POST['post_ID'] : null));
+    $post_id = (isset($_GET['post']) ? $_GET['post'] : (isset($_POST['post_ID']) ? $_POST['post_ID'] : null));
 
     if (($post_id == get_option('page_on_front') ? true : false)) {
-//        $frontpage = new CustomPostType('Page');
+        //        $frontpage = new CustomPostType('Page');
 //        $frontpage->addMetaBox('Contact Info', array(
 //            'phone'   => 'text',
 //            'email'   => 'text',
@@ -43,51 +42,50 @@ if (is_admin()) {
 //            'hours'   => 'textarea'
 //        ));
     }
-
 };
 
-if ( ! function_exists('kmaslim_setup')) :
+if (! function_exists('kmaslim_setup')) :
 
     function kmaslim_setup()
     {
-
         load_theme_textdomain('kmaslim', get_template_directory() . '/languages');
         add_theme_support('automatic-feed-links');
         add_theme_support('title-tag');
         add_theme_support('post-thumbnails');
 
-        register_nav_menus(array(
+        register_nav_menus([
             'mobile-menu' => esc_html__('Mobile Menu', 'kmaslim'),
             'footer-menu' => esc_html__('Footer Menu', 'kmaslim'),
             'main-menu'   => esc_html__('Main Navigation', 'kmaslim')
-        ));
+        ]);
 
-        add_theme_support('html5', array(
+        add_theme_support('html5', [
             'search-form',
             'comment-form',
             'comment-list',
             'gallery',
             'caption'
-        ));
+        ]);
 
         function kmaslim_inline()
-        { ?>
+        {
+            ?>
             <style type="text/css">
                 <?php echo file_get_contents(get_template_directory() . '/style.css'); ?>
             </style>
-        <?php }
+        <?php
+        }
 
         add_action('wp_head', 'kmaslim_inline');
 
         add_image_size('large-thumbnail', 300, 300, true);
-
     }
 endif;
 add_action('after_setup_theme', 'kmaslim_setup');
 
 function kmaslim_scripts()
 {
-    wp_register_script('scripts', get_template_directory_uri() . '/app.js', array(), '0.0.1', true);
+    wp_register_script('scripts', get_template_directory_uri() . '/app.js', [], '0.0.1', true);
     wp_enqueue_script('scripts');
 }
 
