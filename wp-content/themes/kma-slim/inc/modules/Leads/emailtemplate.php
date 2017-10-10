fd27a7cffbdc0893b47e538ea2af4b831ccc290f
Daron Adkins <daron.adkins@gmail.com>
2017-10-10 15:18:13 -0500

    wip

---
+++ wp-content/themes/kma-slim/inc/modules/Leads/emailtemplate.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body bgcolor="#EAEAEA" style="background-color:#EAEAEA;">
+<table
+    cellpadding="0"
+    cellspacing="0"
+    border="0"
+    align="center"
+    style="width:650px;
+    background-color:#FFFFFF;
+    margin:30px auto;"
+    bgcolor="#FFFFFF" >
+    <tbody>
+    <tr>
+        <td style="padding:20px; border:1px solid #ddd; border-top:10px solid #219C93; border-bottom: #C3B592 solid 2px;" >
+                    <!--[content]-->
+        </td>
+    </tr>
+    </tbody>
+</table>
+<p style="text-align:center; color:#999; font-family:sans-serif; font-size:12px;">This email was sent by <a href="http://hannoninsurance.com">hannoninsurance.com</a>.</p>
+<p style="text-align:center; color:#999; font-family:sans-serif; font-size:12px;">&copy; 2017 Hannon Insurance.</p>
+</body>
+</html>'
