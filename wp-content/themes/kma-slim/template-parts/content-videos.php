73b2a0f193e57ac799dc9ef8cf16d4e2bfeb70ae
Jack Kerigan <support@kerigan.com>
2017-10-13 18:13:37 -0400

    wip

--- wp-content/themes/kma-slim/template-parts/content-videos.php
+++ wp-content/themes/kma-slim/template-parts/content-videos.php
@@ -16,8 +16,8 @@ include(locate_template('template-parts/partials/top.php'));
             <section class="header section">
                 <div class="header-container">
                     <div class="container">
-                        <h1 class="title is-1"><?php echo $headline; ?></h1>
-                        <?php echo($subhead != '' ? '<p class="subtitle">' . $subhead . '</p>' : null); ?>
+                        <h1 class="title is-1"><?php echo $headline; ?>
+                            <?php echo($subhead != '' ? '<span class="subtitle">' . $subhead . '</span>' : null); ?></h1>
                     </div>
                 </div>
             </section>
