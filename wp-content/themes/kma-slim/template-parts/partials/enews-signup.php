a66db1048dc0a679425832b6dec19e243e285e14
Bryan Baird <bryan@kerigan.com>
2017-11-28 13:51:34 -0500

    subscribe form

--- wp-content/themes/kma-slim/template-parts/partials/enews-signup.php
+++ wp-content/themes/kma-slim/template-parts/partials/enews-signup.php
@@ -8,19 +8,22 @@
 ?>
 <div class="enewsletter-signup">
     <div class="container">
-        <form method="get" action="#" class="form columns" >
+        <form action="https://boneandjointclinicbr.us17.list-manage.com/subscribe/post?u=b5e9771d295b9a44f4aff96a6&amp;id=a8de836e2a" method="post" class="form columns" name="mc-embedded-subscribe-form" novalidate>
             <div class="column is-half-desktop">
                 <p class="form-instructions is-centered">
                     Sign up to receive our quarterly Doctor's Orders e-newsletter.
                 </p>
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
             </div>
             <div class="column is-half-desktop">
                 <div class="field is-grouped">
                     <p class="control is-expanded">
-                        <input class="input is-centered" type="text" placeholder="email address">
+                        <input type="email" value="" name="EMAIL" class="input is-centered" placeholder="email address">
                     </p>
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b5e9771d295b9a44f4aff96a6_a8de836e2a" tabindex="-1" value=""></div>
                     <p class="control">
-                        <button class="button is-primary is-outlined">
+                        <button type="submit" class="button is-primary is-outlined">
                             sign up
                         </button>
                     </p>
