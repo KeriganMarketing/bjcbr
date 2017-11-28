438665d4558c1c9612338c3b74d8b02412d570c3
Bryan Baird <bryan@kerigan.com>
2017-11-28 17:57:52 -0500

    added mailchimp subscription

--- wp-content/themes/kma-slim/template-parts/partials/enews-signup.php
+++ wp-content/themes/kma-slim/template-parts/partials/enews-signup.php
@@ -1,25 +1,31 @@
 <?php
+
+use Includes\Modules\Helpers\MailChimp;
+
 /**
  * @package KMA
  * @subpackage kmaslim
  * @since 1.0
  * @version 1.2
  */
+
+if($_POST['email_address'] != '' && $_POST['b_b5e9771d295b9a44f4aff96a6_a8de836e2a'] == ''){
+    $mailChimp = new MailChimp();
+    $message = $mailChimp->handleSubscriber($_POST['email_address']);
+}
 ?>
-<div class="enewsletter-signup">
+<div class="enewsletter-signup" id="subscribe">
     <div class="container">
-        <form action="https://boneandjointclinicbr.us17.list-manage.com/subscribe/post?u=b5e9771d295b9a44f4aff96a6&amp;id=a8de836e2a" method="post" class="form columns" name="mc-embedded-subscribe-form" novalidate>
+        <form method="post" class="form columns" name="mailchimp_subscribe" novalidate action="#subscribe">
             <div class="column is-half-desktop">
                 <p class="form-instructions is-centered">
-                    Sign up to receive our quarterly Doctor's Orders e-newsletter.
+                    <?= $message != '' ? $message : 'Sign up to receive our quarterly Doctor\'s Orders e-newsletter.'; ?>
                 </p>
-                <div class="response" id="mce-error-response" style="display:none"></div>
-                <div class="response" id="mce-success-response" style="display:none"></div>
             </div>
             <div class="column is-half-desktop">
                 <div class="field is-grouped">
                     <p class="control is-expanded">
-                        <input type="email" value="" name="EMAIL" class="input is-centered" placeholder="email address">
+                        <input type="email" value="" name="email_address" class="input is-centered" placeholder="email address">
                     </p>
                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b5e9771d295b9a44f4aff96a6_a8de836e2a" tabindex="-1" value=""></div>
                     <p class="control">
