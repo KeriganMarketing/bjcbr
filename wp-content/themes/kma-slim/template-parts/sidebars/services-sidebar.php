61ab7ce625a70b9a846e736534af6f7552d540cf
Bryan Baird <bryan@kerigan.com>
2017-12-01 11:02:04 -0500

    Install Google Tag Manager

--- wp-content/themes/kma-slim/template-parts/sidebars/services-sidebar.php
+++ wp-content/themes/kma-slim/template-parts/sidebars/services-sidebar.php
@@ -4,8 +4,8 @@ $pageList = get_children($parentId, [
     'post_parent'    => $parentId,
     'post_type'      => 'page',
     'posts_per_page' => -1,
-    'orderby'        => 'menu_order',
     'order'          => 'ASC',
+    'orderby'        => 'menu_order',
 ]);
 ?>
 <div class="sidebar-module specialties-list">
